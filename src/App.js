40c3f08260ccb8e2aac75e29ff7f1d13be4123df
ellamarie005 <ellatolentino05@gmail.com>
2018-07-11 19:38:58 -0700

    initial server

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">My Name is Ella</div>
+    );
+  }
+}
+
+const header = () => {
+  return <header>I am a header</header>
+}
+
+export default App;
+
+// find website that looks cool and recreate it
