f93a3408525e6aeb2645cbf5a91563323423711a
ellamarie005 <ellatolentino05@gmail.com>
2018-07-12 21:49:37 -0700

    header animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import logo from "assets/Jacob-50.png";
 import HamburgerMenu from "components/ui/HamburgerMenu";
+import logoWhite from "assets/Jacob-white-50.png";
 
 class Header extends Component {
   state = {
@@ -17,7 +18,24 @@ class Header extends Component {
     const { open } = this.state;
     return (
       <HeaderWrapper>
-        <img src={logo} alt="logo" />
+        <nav className="nav__left">
+          <span>
+            <a href="">About</a>
+          </span>
+          <span>
+            <a href="">Portfolio</a>
+          </span>
+        </nav>
+        <img className="logo__blue" src={logo} alt="logo" />
+        <img className="logo__white" src={logoWhite} alt="logo" />
+        <nav className="nav__right">
+          <span>
+            <a href="">UX Talks</a>
+          </span>
+          <span>
+            <a href="">Resume</a>
+          </span>
+        </nav>
         <HamburgerMenu
           onClick={this.toggleMenu}
           className={open ? "open" : ""}
@@ -29,15 +47,69 @@ class Header extends Component {
 
 const HeaderWrapper = styled.header`
   height: 58px;
-  background-color: beige;
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 0 18px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  box-sizing: border-box;
+  z-index: 10;
+  background-color: white;
+
+  nav {
+    display: none;
+
+    span {
+      padding: 15px;
+
+      &:hover {
+        a {
+          opacity: 1;
+          &:after {
+            transform: rotate3d(0, 0, 0, 90deg);
+          }
+        }
+      }
+    }
+
+    a {
+      color: white;
+      text-decoration: none;
+      font-size: 18px;
+      position: relative;
+      opacity: 0.7;
+      transition: opacity 0.4s ease-in-out;
+
+      &:after {
+        content: "";
+        position: absolute;
+        bottom: -7px;
+        left: 0;
+        background-color: white;
+        height: 3px;
+        width: 100%;
+        transform: rotate3d(0, 1, 0, 90deg);
+        pointer-events: none;
+        transition: transform 0.3s ease-in-out;
+      }
+    }
+  }
 
   img {
-    height: 24px;
-    width: 24px;
+    height: 50px;
+    width: 50px;
+
+    &.logo {
+      &__blue {
+        display: block;
+      }
+      &__white {
+        display: none;
+      }
+    }
   }
   /* tablet */
   @media screen and (min-width: 768px) {
@@ -46,6 +118,26 @@ const HeaderWrapper = styled.header`
   @media screen and (min-width: 1024px) {
     flex-direction: row;
     justify-content: center;
+    background-color: transparent;
+    top: 40px;
+
+    nav {
+      display: block;
+    }
+    img {
+      height: 50px;
+      width: 50px;
+      margin: 0 20px;
+
+      &.logo {
+        &__blue {
+          display: none;
+        }
+        &__white {
+          display: block;
+        }
+      }
+    }
   }
 `;
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,33 +6,44 @@ import down from "assets/down.svg";
 
 const Hero = () => {
   return (
-    <HeroWrapper>
-      <BackgroundImage />
-      <Content>
-        <h1>Jacob Heflin</h1>
-        <h3>User Experience Designer</h3>
-        <img className="profile-pic" src={profilePic} alt="Profile Pic" />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          id="Layer_1"
-          width="64"
-          height="64"
-          viewBox="0 0 64 64"
-        >
-          <path
-            strokeLinejoin="bevel"
-            strokeMiterlimit="10"
-            d="M15,24L32,41L49,24"
-            strokeDasharray="49,51"
-          />
-        </svg>
-      </Content>
-    </HeroWrapper>
+    <Content>
+      <Background />
+      <h1>Jacob Heflin</h1>
+      <h3>User Experience Designer</h3>
+      <img className="profile-pic" src={profilePic} alt="Profile Pic" />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        id="Layer_1"
+        width="64"
+        height="64"
+        viewBox="0 0 64 64"
+      >
+        <path
+          strokeLinejoin="bevel"
+          strokeMiterlimit="10"
+          d="M15,24L32,41L49,24"
+          strokeDasharray="49,51"
+        />
+      </svg>
+    </Content>
   );
 };
 
-const Content = styled.div`
+const Background = styled.div`
+  background: url(${bg}) #999;
+  background-size: cover;
+  background-position: center;
+  filter: brightness(0.9);
   position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+`;
+
+const Content = styled.div`
+  position: relative;
   width: 100%;
   height: 100%;
   top: 0;
@@ -42,7 +53,10 @@ const Content = styled.div`
   justify-content: center;
   align-items: center;
   color: white;
+  height: 100vh;
 
+  overflow: hidden;
+  position: relative;
   h1 {
     font-size: 54px;
     margin-bottom: 5px;
@@ -63,20 +77,11 @@ const Content = styled.div`
     stroke: white;
     stroke-width: 2;
   }
-`;
 
-const BackgroundImage = styled.div`
-  background: url(${bg}) #999;
-  background-size: cover;
-  background-position: center;
-  padding-top: calc(9 / 16 * 100%);
-  filter: brightness(0.9);
-`;
-
-const HeroWrapper = styled.div`
-  /* max-height: 850px; */
-  overflow: hidden;
-  position: relative;
+  @media screen and (min-width: 1024px) {
+    height: 70vh;
+    min-height: 700px;
+  }
 `;
 
 export default Hero;
