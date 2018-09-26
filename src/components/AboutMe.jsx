402250671147b9040a9751c2e8214ca862c6ba15
ellamarie005 <ellatolentino05@gmail.com>
2018-09-25 23:19:16 -0700

    added some about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,7 +8,10 @@ class AboutMe extends Component {
         <Row className='row'>
           <div className='col-md-6'>
             <h1>AB<span>OU</span>T ME</h1>
-            <p>I am a Research Assistant turned Software Developer.</p>
+            <p>I am a Research Assistant turned Software Developer. I've always been the type of person that loves taking up hobbies, and one of the things needed to be checked off of my bucket list was to "Learn how to code". Basics, of course.
+              I didn't know that I would enjoy it so much, and so I decided to take it up as a permanent hobby. As I continued learning, my drive and passion for coding kept on growing, which then brings me to where I am.
+              After a year of self-learning, I've decided to take a plunge into Software Development.
+            </p>
             <br />
             <p>During my free time, I enjoy...</p>
           </div>
@@ -40,6 +43,10 @@ h1 {
 }
 h1 span {
   color: #ff0500;
+}
+
+p {
+  padding: 30px;
 }
 `
 
