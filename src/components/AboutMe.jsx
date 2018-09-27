d807716aa09ecf25de5af4798e9491245ad2068f
ellamarie005 <ellatolentino05@gmail.com>
2018-09-26 23:43:41 -0700

    added a little bit more about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,12 +8,16 @@ class AboutMe extends Component {
         <Row className='row'>
           <div className='col-md-6'>
             <h1>AB<span>OU</span>T ME</h1>
-            <p>I am a Research Assistant turned Software Developer. I've always been the type of person that loves taking up hobbies, and one of the things needed to be checked off of my bucket list was to "Learn how to code". Basics, of course.
-              I didn't know that I would enjoy it so much, and so I decided to take it up as a permanent hobby. As I continued learning, my drive and passion for coding kept on growing, which then brings me to where I am.
+            <p>I am a Research Assistant turned Software Developer. I've always been the type of person that loves taking up hobbies, 
+              and one of the things needed to be checked off of my bucket list was to "Learn how to code". Basics, of course.
+              I didn't know that I would enjoy it so much, and so I decided to take it up as a permanent hobby. 
+              As I continued learning, my drive and passion for coding kept on growing, which then brings me to where I am.
               After a year of self-learning, I've decided to take a plunge into Software Development.
             </p>
             <br />
-            <p>During my free time, I enjoy...</p>
+            <p>During my free time, I enjoy karaoke-ing, playing some mobile games, traveling, and crossing things out of my bucket list.
+              I'm a big enthusiast when it comes to self-growth, which is why I try my best to do activities in my bucket list.
+              </p>
           </div>
           <div className='col-md'>
             Picture
